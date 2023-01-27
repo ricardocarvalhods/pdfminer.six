37c1bd2a0e1d80111d2b3602e40596a586df038a
Ricardo Carvalho <ricardosc@gmail.com>
2023-01-27 15:00:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
     readme = f.read()
 
 setup(
-    name="pdfminer.six",
+    name="pdfminer",
     version=package.__version__,
     packages=["pdfminer"],
     package_data={"pdfminer": ["cmap/*.pickle.gz", "py.typed"]},
